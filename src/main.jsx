f35fe4d781927dd6114c9a8ea478c045140e23eb
Amirbek <amirbek.norov16@gmail.com>
2025-08-12 15:05:01 +0500

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router'
 
-// fghgf
+
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
    <App />
